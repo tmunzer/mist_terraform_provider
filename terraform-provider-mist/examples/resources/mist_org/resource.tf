51a730c3353387d45cd1b5a2ffae6ed3676e05e6
Thomas Munzer <tmunzer@juniper.net>
2024-07-13 16:11:23 +0200

    migrating provider

--- terraform-provider-mist/examples/resources/mist_org/resource.tf
+++
@@ -1,3 +0,0 @@
-resource "mist_org" "org_one" {
-  name = "Org One"
-}
--- terraform-provider-mist/examples/resources/mist_org_nactag/resource.tf
+++
@@ -1,9 +0,0 @@
-resource "mist_org_nactag" "tag_one" {
-  name   = "tag_one"
-  type   = "match"
-  match  = "client_mac"
-  org_id = mist_org.terraform_test.id
-  values = [
-    "5c5b35*"
-  ]
-}
