1901b3f979b183e4114d32e57630dbcd9ace5e40
Thomas Munzer <tmunzer@juniper.net>
2024-07-13 12:23:59 +0200

    update specs

--- test_datasource/main.tf
+++ test_datasource/main.tf
@@ -18,15 +18,15 @@ resource "mist_org" "terraform_test" {
   name = "Terraform Testing"
 }
 
-# data "mist_device_ap_stats" "example" {
-#   #org_id = mist_org.terraform_test.id
-#   org_id  = "9777c1a0-6ef6-11e6-8bbf-02e208b2d34f"
-# }
-
-data "mist_device_switch_stats" "example2" {
+data "mist_device_ap_stats" "example" {
   #org_id = mist_org.terraform_test.id
   org_id  = "9777c1a0-6ef6-11e6-8bbf-02e208b2d34f"
 }
+
+# data "mist_device_switch_stats" "example2" {
+#   #org_id = mist_org.terraform_test.id
+#   org_id  = "9777c1a0-6ef6-11e6-8bbf-02e208b2d34f"
+# }
 # data "mist_device_gateway_stats" "example3" {
 #   org_id  = "9777c1a0-6ef6-11e6-8bbf-02e208b2d34f"
 # }
